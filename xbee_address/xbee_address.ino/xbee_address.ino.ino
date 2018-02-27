0035ae457ec5b08edb45485230bab99334021cca
rpg2014 <pgiven14@gmail.com>
2018-02-26 20:18:10 -0500

    moving to laptop

---
+++ xbee_address/xbee_address.ino/xbee_address.ino.ino
@@ -0,0 +1,60 @@
+/*
+Example from Arduino SoftwareSerial tutorial
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3, 2); // RX, TX
+
+String id = String(3331);
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  mySerial.listen();
+  delay(500);
+  Serial.println("Sending +++");
+  mySerial.write("+++");
+  delay(1000);
+  while(mySerial.available()){
+    Serial.write(mySerial.read());
+  }
+  Serial.println("\nSetting id to " + id);
+  
+  mySerial.write("ATID3331\r");
+  delay(500);
+  while(mySerial.available()){
+    Serial.write(mySerial.read());
+  }
+  Serial.print("\nSet id to ");
+  mySerial.write("ATID\r");
+  delay(500);
+  while(mySerial.available()){
+    Serial.write(mySerial.read());
+  }
+
+  Serial.print("SH is ");
+  mySerial.write("ATSH\r");
+  while(mySerial.available()){
+    Serial.write(mySerial.read());
+  }
+
+  Serial.print("SL is ");
+  mySerial.write("ATSL\r");
+  while(mySerial.available()){
+    Serial.write(mySerial.read());
+  }
+  
+  
+}
+
+void loop() // run over and over
+{
+  
+  if (mySerial.available())
+    Serial.write(mySerial.read());
+  
+  if (Serial.available())
+    mySerial.write(Serial.read());
+    
+}
